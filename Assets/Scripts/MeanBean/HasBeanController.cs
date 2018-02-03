3b066e8046a423c884d5ec296e1634dc1141fc9f
jonathanholt <jonathanholt1989@gmail.com>
2018-02-03 14:29:23 +0000

    HasBean animation changes 3

--- Assets/Scripts/MeanBean/HasBeanController.cs
+++ Assets/Scripts/MeanBean/HasBeanController.cs
@@ -6,19 +6,32 @@ public class HasBeanController : MonoBehaviour {
 
 	private static Animator anim;
 	public static int whichAnimation;
+	public static int animationsCount;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+		animationsCount = 10;
+		whichAnimation = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+	public static void chooseAnimation(){
+		if (whichAnimation == 0) {
+			int animationsCountAmended = animationsCount;
+			int randomAnimation = Random.Range (0, animationsCountAmended);
+			changeAnimation (1);
+		} else {
+			changeAnimation (0);
+		}
+	}
 
 	public static void changeAnimation(int animation){
+		Debug.Log("Animation = "+animation);
 		whichAnimation = animation;
-		anim.SetInteger ("Animation", 1);
+		anim.SetInteger ("Animation", animation);
 	}
 }
--- Assets/Scripts/MeanBean/PlayerController.cs
+++ Assets/Scripts/MeanBean/PlayerController.cs
@@ -388,7 +388,7 @@ public class PlayerController : MonoBehaviour {
 
 		ScoreManager.AddPoints (10);
 		ScoreManager.AlterGUI ();
-		HasBeanController.changeAnimation (1);
+		HasBeanController.chooseAnimation ();
 		List<GameObject> affectedSquares = new List<GameObject>();
 		matches = culprits;
 		foreach(GameObject newgameobject in culprits){
