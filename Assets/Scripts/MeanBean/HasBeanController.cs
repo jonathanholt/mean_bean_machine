ac965fee1778406d882ac88b593e4b0c058f8f89
jonathanholt <jonathanholt1989@gmail.com>
2018-02-04 17:16:49 +0000

    HasBean animations now change every 3 matches; Fixes #1

--- Assets/Scripts/MeanBean/HasBeanController.cs
+++ Assets/Scripts/MeanBean/HasBeanController.cs
@@ -7,30 +7,29 @@ public class HasBeanController : MonoBehaviour {
 	private static Animator anim;
 	public static int whichAnimation;
 	public static int animationsCount;
+	public static int newAnimationNow;
 
-	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		animationsCount = 10;
 		whichAnimation = 0;
+		newAnimationNow = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 	public static void chooseAnimation(){
+		newAnimationNow++;
 		if (whichAnimation == 0) {
 			int animationsCountAmended = animationsCount;
 			int randomAnimation = Random.Range (0, animationsCountAmended);
-			changeAnimation (1);
+			changeAnimation (randomAnimation);
 		} else {
-			changeAnimation (0);
+			if(newAnimationNow%3 ==0)
+				changeAnimation (0);
 		}
 	}
 
 	public static void changeAnimation(int animation){
-		Debug.Log("Animation = "+animation);
+		Debug.Log ("Animation..."+animation);
 		whichAnimation = animation;
 		anim.SetInteger ("Animation", animation);
 	}
