85d78bc5ce54cae443329cbe94b16ba820170ba0
jonathanholt <jholt1989@hotmail.co.uk>
2018-09-05 21:17:33 +0100

    Create Prefabs for the 5 bean colours, and create minimal scripts for bean behaviour

---
+++ Assets/Scripts/MeanBean/New/AvalancheController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvalancheController : MonoBehaviour {
+
+	public int avalancheCount;
+	
+	void Start () {
+		avalancheCount = 0;
+	}
+	
+	public void incrementAvalancheCount(){
+		avalancheCount += 1;
+	}
+	
+	public void processAvalanche(){
+		avalancheCount = 0;
+	}
+}
---
+++ Assets/Scripts/MeanBean/New/MotionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionController : MonoBehaviour {
+
+	public bool inMotion;
+	
+	void Start () {
+		inMotion = true;	
+	}
+	
+	public void setMotion(bool motion){
+		inMotion = motion;
+	}
+
+	public bool getMotion(){
+		return inMotion;
+	}
+}
