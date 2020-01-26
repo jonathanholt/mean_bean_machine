dc8789316caabb317207abff1f36169f50e42f22
jonathanholt <jholt1989@hotmail.co.uk>
2020-01-26 18:00:35 +0000

    Starting work on the game over screen, including coding a rotation function to apply to the 'back light' on the game over screen

---
+++ Assets/Scripts/GameOver/RotationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour
+{
+	
+	public int targetDegree;
+	public float speed;
+	private int originalTargetDegree;
+	
+	void Start () {
+		this.originalTargetDegree = targetDegree;
+	}
+
+    void Update()
+    {
+		if(targetDegree < 180 && (int)transform.rotation.eulerAngles.z != targetDegree){
+			transform.Rotate(Vector3.forward * + speed);
+		}
+		else if(targetDegree > 180 && (int)transform.rotation.eulerAngles.z != targetDegree){
+			transform.Rotate(Vector3.forward * - speed);
+		}
+		else{
+			if(targetDegree == this.originalTargetDegree){
+				targetDegree = 360 - targetDegree;
+			}
+			else{
+				targetDegree = this.originalTargetDegree;
+			} 
+		}
+    }
+}
