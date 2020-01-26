a895e7f990be2d5ae0718bee0bb4e36ecb55240f
jonathanholt <jholt1989@hotmail.co.uk>
2020-01-26 19:01:46 +0000

    Adding gradient and tickertape effect to the text on the game over screen

---
+++ Assets/Scripts/GameOver/GradientController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientController : MonoBehaviour
+{
+	
+	public Gradient gradient;
+	private SpriteRenderer spriteRenderer;
+    private float strobeDuration = 2f;
+	
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+		float t = Mathf.PingPong(Time.time / this.strobeDuration, 1f);
+		spriteRenderer.color = gradient.Evaluate(t);
+    }
+}
---
+++ Assets/Scripts/GameOver/TickerTapeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TickerTapeController : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private Vector3 target;
+    private Vector3 position;
+	private Vector3 originalPosition;
+	public GameObject gameObjectInOriginalPosition;
+
+    void Start()
+    {
+        target = new Vector3(-20.0f, 2.5f, 0f);
+        position = gameObject.transform.position;
+		originalPosition = gameObjectInOriginalPosition.transform.position;
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+		if(transform.position == target){
+			transform.position = originalPosition;
+		}
+    }
+}
