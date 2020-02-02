cf5f60d9ea3508fba92c61c93678389d22617484
jonathanholt <jholt1989@hotmail.co.uk>
2020-02-02 14:52:53 +0000

    Adding a ranting/laughing Dr. Robotnik to the game over screen

---
+++ Assets/Scripts/GameOver/RobotnikLaughingObserver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotnikLaughingObserver : MonoBehaviour
+{
+	
+	public bool countdownReachedOne;
+	public GameObject robotnik;
+	Animator anim;
+	
+    void Start()
+    {
+        countdownReachedOne = false;
+		anim = robotnik.GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+		if(countdownReachedOne){
+			anim.SetTrigger("RobotnikLaughing");
+			countdownReachedOne = false;
+		}
+	}
+}
