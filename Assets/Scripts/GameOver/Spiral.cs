1581153cb80d2da901179c82fc2a2b2686c815d1
jonathanholt <jholt1989@hotmail.co.uk>
2020-02-15 21:02:24 +0000

    Add a spiral motion to the Game Over font

---
+++ Assets/Scripts/GameOver/Spiral.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spiral : MonoBehaviour 
+ {
+ 
+     public float RotateSpeed = 7f;
+     private float Radius = 6f;
+ 
+     private Vector2 _centre;
+     private float _angle;
+	 public GameObject targetGameobject;
+ 
+     private void Start()
+     {
+         _centre = targetGameobject.transform.position;
+     }
+ 
+     private void Update()
+     {
+		this.Radius -= 0.04f;
+		if(this.Radius >= 0f){
+			_angle += RotateSpeed * Time.deltaTime;
+ 
+			var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+			transform.position = _centre + offset;
+		}
+	 }
+  
+  
+ 
+ }
